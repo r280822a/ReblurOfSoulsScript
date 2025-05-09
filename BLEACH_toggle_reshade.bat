cc6fd6b1f2346e6f28c37d3b1b01e5d9c409068e
Rehan Ahmad <r280822a@gmail.com>
2025-05-09 16:34:44 +0100

    Add message stating whether you can play online or not

--- BLEACH_toggle_reshade.bat
+++ BLEACH_toggle_reshade.bat
@@ -1,29 +1,13 @@
 @echo off
 setlocal
 
-REM Check if dxgi.dll exists
-if exist "dxgi.dll" (
-    echo ===============================================================
-    echo ===                   DISABLING RESHADE                     ===
-    echo ===============================================================
-    ren "dxgi.dll" "dxgi 1.dll"
-    echo Renamed dxgi.dll to dxgi 1.dll
+REM Check if reshade disabled (if dxgi 1.dll exists)
+if exist "dxgi 1.dll" (
+    echo ==============================================================
+    echo ===                   Enabling Reshade                     ===
+    echo ===                  CANNOT play online                    ===
+    echo ==============================================================
 
-    REM Run install_EAC.bat
-    if exist "EasyAntiCheat\install_EAC.bat" (
-        call "EasyAntiCheat\install_EAC.bat"
-    ) else (
-        echo install_EAC.bat not found.
-    )
-
-    REM Update desktop shortcut icon
-    powershell -ExecutionPolicy Bypass -File "%~dp0BLEACH_toggle_update_icon.ps1" disabled
-
-REM Else, check if dxgi 1.dll exists
-) else if exist "dxgi 1.dll" (
-    echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    echo +++                   ENABLING RESHADE                     +++
-    echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     ren "dxgi 1.dll" "dxgi.dll"
     echo Renamed dxgi 1.dll back to dxgi.dll
 
@@ -34,8 +18,28 @@ REM Else, check if dxgi 1.dll exists
         echo uninstall_EAC.bat not found.
     )
 
-    REM Update desktop shortcut icon
+    REM Create desktop shortcut and update icon
     powershell -ExecutionPolicy Bypass -File "%~dp0BLEACH_toggle_update_icon.ps1" enabled
+
+REM Else, check if reshade enabled (if dxgi.dll exists)
+) else if exist "dxgi.dll" (
+    echo ===============================================================
+    echo ===                   Disabling Reshade                     ===
+    echo ===                    Can play online                      ===
+    echo ===============================================================
+
+    ren "dxgi.dll" "dxgi 1.dll"
+    echo Renamed dxgi.dll to dxgi 1.dll
+
+    REM Run install_EAC.bat
+    if exist "EasyAntiCheat\install_EAC.bat" (
+        call "EasyAntiCheat\install_EAC.bat"
+    ) else (
+        echo install_EAC.bat not found.
+    )
+
+    REM Create desktop shortcut and update icon
+    powershell -ExecutionPolicy Bypass -File "%~dp0BLEACH_toggle_update_icon.ps1" disabled
 
 ) else (
     echo Neither dxgi.dll nor dxgi 1.dll found.
